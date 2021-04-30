ee3518cbe65a8c97384b5767dc1d1712ff06bfcf
Shingo.sasaki <shingo.sasaki@studist.jp>
2021-05-01 01:37:36 +0900

    不要コードを削除

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
-import * as dotenv from 'dotenv'
 import { loadTemplate } from './template'
 import { createIncomes, createPayment, privateZaimClient, publicZaimClient } from './zaim'
-
-dotenv.config()
 
 const publicTemplate = loadTemplate('publicTemplate.json')
 const privateTemplate = loadTemplate('privateTemplate.json')
